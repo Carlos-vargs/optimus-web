2c50f8a657295c1fd76af5162f1d3d6f5489496a
Carlos-vargs <cvargaslopez769@gmail.com>
2023-11-24 03:01:46 -0600

    ci: add crud for equipos controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,21 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->get('/equipos', 'EquiposController::index');
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// ...
+
+/*
+ * Rutas para el controlador EquiposController
+ */
+$routes->group('equipos', function ($routes) {
+  $routes->get('/', 'EquiposController::index'); // Lista todos los equipos
+  $routes->post('create', 'EquiposController::create'); // Crea un nuevo equipo
+  $routes->get('(:num)', 'EquiposController::show/$1'); // Muestra un equipo específico por id
+  $routes->put('update/(:num)', 'EquiposController::update/$1'); // Actualiza un equipo específico por id
+  $routes->delete('delete/(:num)', 'EquiposController::delete/$1'); // Elimina un equipo específico por id
+});
--- app/Controllers/EquiposController.php
+++ app/Controllers/EquiposController.php
@@ -24,6 +24,20 @@ class EquiposController extends ResourceController
         return $this->respond($equipos);
     }
 
+    public function show($id = null)
+    {
+        $equipoModel = new Equipo();
+
+        $equipo = $equipoModel->find($id);
+        if (!$equipo) {
+            return $this->failNotFound('Equipo no encontrado con ID: ' . $id);
+        }
+
+        $equipo['componentes'] = $equipoModel->getComponentes($id);
+
+        return $this->respond($equipo);
+    }
+
     public function create()
     {
         $rules = [
@@ -36,14 +50,13 @@ class EquiposController extends ResourceController
             'codigoInventario' => 'required|alpha_numeric_space|max_length[50]|is_unique[equipos.codigoInventario]',
         ];
 
-        $data = $this->request->getPost();
-
-        if (!$this->validate($rules)) {
+        $json = $this->request->getJSON(true);
+        if (!$this->validate($rules, $json)) {
             return $this->fail($this->validator->getErrors());
         }
 
         try {
-            $this->model->insert($data);
+            $this->model->insert($json);
             $id = $this->model->getInsertID();
             return $this->respondCreated($this->model->find($id));
         } catch (Exception $e) {
@@ -60,26 +73,27 @@ class EquiposController extends ResourceController
             'fechaDeAdquisicion' => 'required|valid_date',
             'ultimoMantenimiento' => 'permit_empty|valid_date',
             'proximoMantenimiento' => 'permit_empty|valid_date',
-            'codigoInventario' => 'required|alpha_numeric_space|max_length[50]|is_unique[equipos.codigoInventario,id,{id}]',
+            'codigoInventario' => "required|alpha_numeric_space|max_length[50]|is_unique[equipos.codigoInventario,id,{$id}]",
         ];
 
-        $data = $this->request->getRawInput();
+        $json = $this->request->getJSON(true);
 
         if (!$this->model->find($id)) {
-            return $this->failNotFound('Equipo no encontrado');
+            return $this->failNotFound('Equipo no encontrado con ID: ' . $id);
         }
 
-        if (!$this->validate($rules)) {
+        if (!$this->validate($rules, $json)) {
             return $this->fail($this->validator->getErrors());
         }
 
         try {
-            $this->model->update($id, $data);
+            $this->model->update($id, $json);
             return $this->respond($this->model->find($id));
         } catch (Exception $e) {
             return $this->failServerError($e->getMessage());
         }
     }
+
 
     public function delete($id = null)
     {

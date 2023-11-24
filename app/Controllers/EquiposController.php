8bbf75e5b558e4b8c5ac05908caf0bfd5817dceb
Carlos-vargs <cvargaslopez769@gmail.com>
2023-11-24 02:47:25 -0600

    ci: add controller methods for Equipos

--- app/Controllers/EquiposController.php
+++ app/Controllers/EquiposController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\Equipo;
 use CodeIgniter\RESTful\ResourceController;
+use Exception;
 
 class EquiposController extends ResourceController
 {
@@ -21,5 +22,75 @@ class EquiposController extends ResourceController
         }
 
         return $this->respond($equipos);
+    }
+
+    public function create()
+    {
+        $rules = [
+            'idTipoEquipo' => 'required|integer',
+            'idMarca' => 'required|integer',
+            'idEstado' => 'required|integer',
+            'fechaDeAdquisicion' => 'required|valid_date',
+            'ultimoMantenimiento' => 'permit_empty|valid_date',
+            'proximoMantenimiento' => 'permit_empty|valid_date',
+            'codigoInventario' => 'required|alpha_numeric_space|max_length[50]|is_unique[equipos.codigoInventario]',
+        ];
+
+        $data = $this->request->getPost();
+
+        if (!$this->validate($rules)) {
+            return $this->fail($this->validator->getErrors());
+        }
+
+        try {
+            $this->model->insert($data);
+            $id = $this->model->getInsertID();
+            return $this->respondCreated($this->model->find($id));
+        } catch (Exception $e) {
+            return $this->failServerError($e->getMessage());
+        }
+    }
+
+    public function update($id = null)
+    {
+        $rules = [
+            'idTipoEquipo' => 'required|integer',
+            'idMarca' => 'required|integer',
+            'idEstado' => 'required|integer',
+            'fechaDeAdquisicion' => 'required|valid_date',
+            'ultimoMantenimiento' => 'permit_empty|valid_date',
+            'proximoMantenimiento' => 'permit_empty|valid_date',
+            'codigoInventario' => 'required|alpha_numeric_space|max_length[50]|is_unique[equipos.codigoInventario,id,{id}]',
+        ];
+
+        $data = $this->request->getRawInput();
+
+        if (!$this->model->find($id)) {
+            return $this->failNotFound('Equipo no encontrado');
+        }
+
+        if (!$this->validate($rules)) {
+            return $this->fail($this->validator->getErrors());
+        }
+
+        try {
+            $this->model->update($id, $data);
+            return $this->respond($this->model->find($id));
+        } catch (Exception $e) {
+            return $this->failServerError($e->getMessage());
+        }
+    }
+
+    public function delete($id = null)
+    {
+        try {
+            if ($this->model->delete($id)) {
+                return $this->respondDeleted(['id' => $id, 'message' => 'Equipo eliminado']);
+            } else {
+                return $this->failNotFound('Equipo no encontrado');
+            }
+        } catch (Exception $e) {
+            return $this->failServerError($e->getMessage());
+        }
     }
 }
